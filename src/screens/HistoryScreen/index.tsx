ae5f15d7091dd4b4d0140bc2b0636a52a837dad3
Eric Marangon <marangoneric@hotmail.com>
2023-07-30 08:06:30 -0300

    Initial commit

---
+++ src/screens/HistoryScreen/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+export function HistoryScreen() {
+  return (
+    <Container>
+      <Title>HistoryScreen</Title>
+    </Container>
+  );
+}
